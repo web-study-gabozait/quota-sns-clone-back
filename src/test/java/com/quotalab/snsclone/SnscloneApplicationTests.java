4dac3feb227e91abd2072db842837d1d272c6c48
임동현 <79711744+ldh3907@users.noreply.github.com>
2023-07-17 19:09:41 +0900

    init

---
+++ src/test/java/com/quotalab/snsclone/SnscloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quotalab.snsclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnscloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
